6b694266427f2183014e19c9fa90b48663ee6682
WarriorsSami <sbarbutdica@gmail.com>
2021-08-05 13:10:55 +0300

    implemented Room DB via Singleton DP

---
+++ app/src/main/java/com/example/weatherforecastmvvm/data/db/ForecastDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.weatherforecastmvvm.data.db
+
+import android.content.Context
+import androidx.room.*
+import com.example.weatherforecastmvvm.data.db.entity.CurrentWeatherEntry
+
+
+@Database (
+    entities = [CurrentWeatherEntry::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ForecastDatabase: RoomDatabase() {
+    abstract fun currentWeatherDAO(): CurrentWeatherDAO
+
+    // Singleton Design Pattern is used here to preserve the existence of only one DB connection
+    companion object {
+        @Volatile private var instance: ForecastDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                ForecastDatabase::class.java, "forecast.db")
+                .build()
+    }
+}
