4c800293b645dd778e9e94d180ecbab795b7ad3d
WarriorsSami <sbarbutdica@gmail.com>
2021-08-05 13:10:09 +0300

    added DAO support

---
+++ app/src/main/java/com/example/weatherforecastmvvm/data/db/CurrentWeatherDAO.kt
@@ -0,0 +1,20 @@
+package com.example.weatherforecastmvvm.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.weatherforecastmvvm.data.db.entity.CURRENT_WEATHER_ID
+import com.example.weatherforecastmvvm.data.db.entity.CurrentWeatherEntry
+import com.example.weatherforecastmvvm.data.db.unitlocalized.MetricCurrentWeatherEntry
+
+
+@Dao
+interface CurrentWeatherDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(weatherEntry: CurrentWeatherEntry)
+
+    @Query("SELECT * FROM current_weather WHERE id = $CURRENT_WEATHER_ID")
+    fun getWeatherMetric(): LiveData<MetricCurrentWeatherEntry>
+}
