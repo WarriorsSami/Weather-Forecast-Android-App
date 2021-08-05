d98575939f8801990f5566dd9750e0f6a0377da8
WarriorsSami <sbarbutdica@gmail.com>
2021-08-05 21:23:12 +0300

    test weather network data source abstraction in current weather fragment via observer DP

--- app/src/main/java/com/example/weatherforecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/weatherforecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,14 +1,17 @@
 package com.example.weatherforecastmvvm.ui.weather.current
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.weatherforecastmvvm.R
-import com.example.weatherforecastmvvm.data.WeatherStackAPIService
+import com.example.weatherforecastmvvm.data.network.ConnectivityInterceptorImpl
+import com.example.weatherforecastmvvm.data.network.WeatherNetworkDataSourceImpl
+import com.example.weatherforecastmvvm.data.network.WeatherStackAPIService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -33,11 +36,16 @@ class CurrentWeatherFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
 
-        val weatherAPIService = WeatherStackAPIService()
-        GlobalScope.launch(Dispatchers.Main) {
-            val currentWeatherResponse = weatherAPIService.getCurrentWeatherAsync("Calafat").await()
+        val weatherAPIService = WeatherStackAPIService(ConnectivityInterceptorImpl(this.requireContext()))
+        val weatherNetworkDataSource = WeatherNetworkDataSourceImpl(weatherAPIService)
+
+        weatherNetworkDataSource.downloadedCurrentWeather.observe(viewLifecycleOwner, Observer {
             val textView = activity?.findViewById<TextView>(R.id.textView)
-            textView?.text = currentWeatherResponse.currentWeatherEntry.toString()
+            textView?.text = it.currentWeatherEntry.toString()
+        })
+
+        GlobalScope.launch(Dispatchers.Main) {
+            weatherNetworkDataSource.fetchCurrentWeather("Calafat", "en")
         }
     }
 
