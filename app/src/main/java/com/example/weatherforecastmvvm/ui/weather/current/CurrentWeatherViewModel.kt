a2bc97221496341abae02cc024a414b98baee193
WarriorsSami <sbarbutdica@gmail.com>
2021-08-12 20:56:53 +0300

    changed gif

--- app/src/main/java/com/example/weatherforecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/example/weatherforecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 class CurrentWeatherViewModel(
     private val forecastRepository: ForecastRepository
 ): ViewModel() {
-    private val unitSystem = UnitSystem.IMPERIAL
+    private val unitSystem = UnitSystem.METRIC
 
     val isMetric: Boolean
         get() = unitSystem == UnitSystem.METRIC
