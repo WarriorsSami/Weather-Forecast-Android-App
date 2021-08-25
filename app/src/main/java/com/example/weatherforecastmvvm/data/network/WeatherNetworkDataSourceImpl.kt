50bd58a9117ece3129052015d36703f5332f4636
WarriorsSami <sbarbutdica@gmail.com>
2021-08-25 18:46:42 +0300

    feature(weather network): fetch future weather
    I've implemented fetch method for future weather response in weather network data source using live data and backing properties to make the repository aware when some new future weather data has been received.

--- app/src/main/java/com/example/weatherforecastmvvm/data/network/WeatherNetworkDataSourceImpl.kt
+++ app/src/main/java/com/example/weatherforecastmvvm/data/network/WeatherNetworkDataSourceImpl.kt
@@ -4,8 +4,12 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.weatherforecastmvvm.data.network.response.CurrentWeatherResponse
+import com.example.weatherforecastmvvm.data.network.response.FutureWeatherResponse
 import com.example.weatherforecastmvvm.internal.NoInternetConnectionException
 import retrofit2.HttpException
+
+
+const val FUTURE_WEATHER_DAYS = 3
 
 class WeatherNetworkDataSourceImpl (
     private val WeatherAPIService: WeatherAPIService
@@ -21,6 +25,23 @@ class WeatherNetworkDataSourceImpl (
                 .getCurrentWeatherAsync(location, languageCode)
                 .await()
             _downloadedCurrentWeather.postValue(fetchedCurrentWeather)
+        } catch(err: NoInternetConnectionException) {
+            Log.e("Connectivity", "No internet connection", err)
+        } catch(err: HttpException) {
+            Log.e("HTTP", "400 Bad Request", err)
+        }
+    }
+
+    private val _downloadedFutureWeather = MutableLiveData<FutureWeatherResponse>()
+    override val downloadedFutureWeather: LiveData<FutureWeatherResponse>
+        get() = _downloadedFutureWeather
+
+    override suspend fun fetchFutureWeather(location: String, languageCode: String) {
+        try {
+            val fetchedFutureWeather = WeatherAPIService
+                .getFutureWeatherAsync(location, FUTURE_WEATHER_DAYS, languageCode)
+                .await()
+            _downloadedFutureWeather.postValue(fetchedFutureWeather)
         } catch(err: NoInternetConnectionException) {
             Log.e("Connectivity", "No internet connection", err)
         } catch(err: HttpException) {
