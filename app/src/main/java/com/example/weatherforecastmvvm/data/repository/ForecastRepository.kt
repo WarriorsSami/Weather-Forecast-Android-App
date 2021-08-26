0b763f8a87ca6ea4043d39d3038f3c42088389cd
WarriorsSami <sbarbutdica@gmail.com>
2021-08-26 20:49:15 +0300

    feat(forecast repo): add future weather
    Forecast Repository must provide future weather functionality similar to current weather repository segment such as getFutureWeatherList, initWeatherData for future weather, persistFetchedFutureWeather (+ replace old future entries), fetchFutureWeather and isFetchFutureNeeded.

--- app/src/main/java/com/example/weatherforecastmvvm/data/repository/ForecastRepository.kt
+++ app/src/main/java/com/example/weatherforecastmvvm/data/repository/ForecastRepository.kt
@@ -3,9 +3,13 @@ package com.example.weatherforecastmvvm.data.repository
 import androidx.lifecycle.LiveData
 import com.example.weatherforecastmvvm.data.db.entity.WeatherLocation
 import com.example.weatherforecastmvvm.data.db.unitlocalized.current.UnitSpecificCurrentWeatherEntry
+import com.example.weatherforecastmvvm.data.db.unitlocalized.future.UnitSpecificSimpleFutureWeatherEntry
+import org.threeten.bp.LocalDate
 
 interface ForecastRepository {
     suspend fun getCurrentWeather(metric: Boolean): LiveData<out UnitSpecificCurrentWeatherEntry>
+
+    suspend fun getFutureWeatherList(startDate: LocalDate, metric: Boolean): LiveData<out List<UnitSpecificSimpleFutureWeatherEntry>>
 
     suspend fun getWeatherLocation(): LiveData<WeatherLocation>
 }
