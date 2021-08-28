7e4c177286460bcd2b9e128a97cf3a6e39fb3257
WarriorsSami <sbarbutdica@gmail.com>
2021-08-28 19:28:53 +0300

    feat(list item): configure day item class
    I've used Groupie android library for defining Recycler View's item class, which holds the UI representation of the future simple weather entry including condition_text, date, temperature and condition_icon.

---
+++ app/src/main/java/com/example/weatherforecastmvvm/ui/weather/future/list/FutureWeatherItem.kt
@@ -0,0 +1,43 @@
+package com.example.weatherforecastmvvm.ui.weather.future.list
+
+import com.example.weatherforecastmvvm.R
+import com.example.weatherforecastmvvm.data.db.unitlocalized.future.MetricSimpleFutureWeatherEntry
+import com.example.weatherforecastmvvm.data.db.unitlocalized.future.UnitSpecificSimpleFutureWeatherEntry
+import com.example.weatherforecastmvvm.internal.glide.GlideApp
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.item_future_weather.*
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.FormatStyle
+
+class FutureWeatherItem(
+    private val weatherEntry: UnitSpecificSimpleFutureWeatherEntry
+): Item() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.apply {
+            textView_condition?.text = weatherEntry.conditionText
+            updateDate()
+            updateTemperature()
+            updateConditionIcon()
+        }
+    }
+
+    override fun getLayout() = R.layout.item_future_weather
+
+    private fun ViewHolder.updateDate() {
+        val dtFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+        textView_date?.text = weatherEntry.date.format(dtFormatter)
+    }
+
+    private fun ViewHolder.updateTemperature() {
+        val unitAbbreviation = if (weatherEntry is MetricSimpleFutureWeatherEntry) "°C" else "°F"
+        textView_temperature?.text = "${weatherEntry.avgTemperature}$unitAbbreviation"
+    }
+
+    private fun ViewHolder.updateConditionIcon() {
+        GlideApp.with(this.containerView)
+            .load("http:" + weatherEntry.conditionIconUrl)
+            .into(imageView_condition_icon)
+    }
+}
