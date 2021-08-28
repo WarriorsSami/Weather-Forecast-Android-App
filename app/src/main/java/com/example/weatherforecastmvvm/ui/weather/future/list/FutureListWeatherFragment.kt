516faaacb325a052d0eed0fce89ead8781d30bd0
WarriorsSami <sbarbutdica@gmail.com>
2021-08-28 18:47:41 +0300

    feat(future UI): update list fragment
    I've updated future list weather fragment by linking correspondent view models and creating bindUI procedure. Also, it must be noticed that some proper xml layouts for future list weather and item future weather have been added.

--- app/src/main/java/com/example/weatherforecastmvvm/ui/weather/future/list/FutureListWeatherFragment.kt
+++ app/src/main/java/com/example/weatherforecastmvvm/ui/weather/future/list/FutureListWeatherFragment.kt
@@ -6,13 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
+import androidx.lifecycle.Observer
 import com.example.weatherforecastmvvm.R
+import com.example.weatherforecastmvvm.ui.base.ScopedFragment
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class FutureListWeatherFragment : Fragment() {
+class FutureListWeatherFragment : ScopedFragment(), KodeinAware {
 
-    companion object {
-        fun newInstance() = FutureListWeatherFragment()
-    }
+    override val kodein by closestKodein()
+    private val viewModelFactory: FutureListWeatherViewModelFactory by instance()
 
     private lateinit var viewModel: FutureListWeatherViewModel
 
@@ -25,8 +35,37 @@ class FutureListWeatherFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FutureListWeatherViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(FutureListWeatherViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
+    @DelicateCoroutinesApi
+    private fun bindUI() = launch(Dispatchers.Main) {
+        val futureWeatherEntries = viewModel.weatherEntries.await()
+        val weatherLocation = viewModel.weatherLocation.await()
+
+        weatherLocation.observe(viewLifecycleOwner, Observer { location ->
+            if (location == null) return@Observer
+
+            updateLocation(location.name)
+        })
+
+        futureWeatherEntries.observe(viewLifecycleOwner, Observer { weatherEntries ->
+            if (weatherEntries == null) return@Observer
+
+            val groupLoading = activity?.findViewById<Group>(R.id.group_loading)
+            groupLoading?.visibility = View.GONE
+
+            updateDateTo3DaySpan()
+        })
+    }
+
+    private fun updateLocation(location: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateTo3DaySpan() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Next 3 days"
+    }
 }
